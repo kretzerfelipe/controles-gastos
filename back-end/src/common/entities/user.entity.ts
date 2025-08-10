01f0b22f0f62608a53c36c94fdd7ba0216541f8e
kretzerfelipe <felipegk2006@gmail.com>
2025-08-10 19:40:49 -0300

    :tada: init: inicializa projeto nest e configura primeira classe, a de user

---
+++ back-end/src/common/entities/user.entity.ts
@@ -0,0 +1,51 @@
+export class User {
+  private _id: number;
+  private _name: string;
+  private _email: string;
+  private _password: string;
+  private _createdAt: Date;
+
+  constructor(id: number, name: string, email: string, password: string) {
+    this._id = id;
+    this._name = name;
+    this._email = email;
+    this._password = password;
+    this._createdAt = new Date();
+  }
+
+  // Getters
+  get id(): number {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  // Método para retornar dados públicos (sem senha)
+  toPublic(): object {
+    return {
+      id: this._id,
+      name: this._name,
+      email: this._email,
+      createdAt: this._createdAt,
+    };
+  }
+
+  // Método para validar senha no login
+  verificarSenha(senhaInformada: string): boolean {
+    return this._password === senhaInformada;
+  }
+}
