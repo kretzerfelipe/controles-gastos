c0d66b81a053acbe60baced53a8307ebcdbe7214
kretzerfelipe <felipegk2006@gmail.com>
2025-08-11 09:50:23 -0300

    :sparkles: feat: altera para aceita req do front end

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: env.FRONT_END_URL, // Coloque a URL do seu frontend aqui
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // se você precisar enviar cookies ou auth
+  });
+
   await app.listen(3000);
 }
 bootstrap();
