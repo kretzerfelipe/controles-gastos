9f6adb0a8e693386892011d486f6378447f84bd3
kretzerfelipe <felipegk2006@gmail.com>
2025-08-10 21:27:12 -0300

    :sparkles: feat: faz controla basico do form de login

--- front-end/src/pages/login.tsx
+++ front-end/src/pages/login.tsx
@@ -9,11 +9,35 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { PasswordInput } from '@/components/ui/password-input';
+import { PasswordInput } from "@/components/ui/password-input";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6).max(100),
+});
 
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data: z.infer<typeof loginSchema>) => {
+    console.log(data);
+  };
+
   return (
-    <div className="flex-container min-h-dvh bg-background items-center justify-center p-5">
+    <form
+      className="flex-container min-h-dvh bg-background items-center justify-center p-5"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle>Entrar</CardTitle>
@@ -22,24 +46,42 @@ export function Login() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <div>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
-                  type="email"
+                  type="string"
                   placeholder="fernando@email.com"
+                  {...register("email")}
+                  onChange={() => {
+                    clearErrors("email");
+                  }}
                 />
+                {errors.email && (
+                  <p className="text-red-400">
+                    {errors.email.message === "Invalid email address"
+                      ? "Email inválido"
+                      : errors.email.message}
+                  </p>
+                )}
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
                   <Label htmlFor="password">Senha</Label>
                 </div>
-                <PasswordInput id="password" placeholder="******" />
+                <PasswordInput
+                  id="password"
+                  placeholder="******"
+                  {...register("password")}
+                  onChange={() => {
+                    clearErrors("password");
+                  }}
+                />
               </div>
             </div>
-          </form>
+          </div>
         </CardContent>
         <CardFooter className="flex-col gap-2">
           <Button type="submit" className="w-full">
@@ -54,6 +96,6 @@ export function Login() {
           </p>
         </CardFooter>
       </Card>
-    </div>
+    </form>
   );
 }
